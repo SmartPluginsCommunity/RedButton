32e45e3753b047220fd752d1fd07ca63c00d91d8
astepanov-sl <astepanov@sodislab.com>
2022-03-19 21:23:10 +0300

    First pull request

---
+++ src/RedButton.Common.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedButton.Common.Core
+{
+    internal class Class1
+    {
+    }
+}
