e18ad9a6365d9530bdbb1efc933b69b170a78c9c
astepanov-sl <astepanov@sodislab.com>
2022-03-29 15:38:26 +0300

    Added GetAllParts

---
+++ src/Common/RedButton.Common.TeklaStructures/Extensions/AssemblyExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tekla.Structures.Model;
+
+namespace RedButton.Common.TeklaStructures.Extensions
+{
+    public static class AssemblyExtension
+    {
+        /// <summary>
+        /// Get all parts for assembly.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="getMainPart">Надо ли получать главную деталь сборки.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> GetAllParts<T>(this Assembly assembly, bool getMainPart) where T : ModelObject
+        {
+            var result = new List<T>();
+
+            if (getMainPart)
+                result.Add(assembly.GetMainPart() as T);
+
+            var secondariesParts = assembly.GetSecondaries().OfType<T>();
+            result.AddRange(secondariesParts);
+
+            return result.Where(part => part != null);
+        }
+    }
+}
