332d0fb6d9020f071a20a28c29a56a02a1848f8e
Andrei Stepanov <web.andrej@gmail.com>
2022-03-29 19:28:42 +0300

    Added yield return

--- src/Common/RedButton.Common.TeklaStructures/Extensions/AssemblyExtension.cs
+++ src/Common/RedButton.Common.TeklaStructures/Extensions/AssemblyExtension.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Tekla.Structures.Model;
 
 namespace RedButton.Common.TeklaStructures.Extensions
@@ -10,25 +9,23 @@ namespace RedButton.Common.TeklaStructures.Extensions
         /// Get all parts for assembly.
         /// </summary>
         /// <param name="assembly">Assembly</param>
-        /// <param name="getMainPart">Get main part.</param>
+        /// <param name="getMainPart">Get a main part</param>
         /// <returns></returns>
         public static IEnumerable<T> GetAllParts<T>(this Assembly assembly, bool getMainPart) where T : ModelObject
         {
-            var result = new List<T>();
-
             if (getMainPart)
             {
                 if (assembly.GetMainPart() is T mainPart)
-                    result.Add(mainPart);
+                    yield return mainPart;
             }
 
-            foreach (var obj in assembly.GetSecondaries())
+            var secondaries = assembly.GetSecondaries();
+
+            foreach (var obj in secondaries)
             {
                 if(obj is T modelObject)
-                    result.Add(modelObject);
+                    yield return modelObject;
             }
-
-            return result;
         }
     }
 }
