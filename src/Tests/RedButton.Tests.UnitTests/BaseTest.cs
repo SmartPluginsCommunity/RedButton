bd0d369aec80e590f3cfecf0bedb2dd2fb9622ae
Andrei Stepanov <web.andrej@gmail.com>
2022-03-31 09:43:15 +0300

    Added test concept

--- src/Tests/RedButton.Tests.UnitTests/BaseTest.cs
+++ src/Tests/RedButton.Tests.UnitTests/BaseTest.cs
@@ -1,16 +1,67 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
 
 namespace RedButton.Tests.UnitTests
 {
     public abstract class BaseTest
     {
+        private ConcurrentBag<ModelObject> _insertedObjects;
+        
+        protected Model Model;
         /// <summary>
         /// Initial logic
         /// </summary>
         [TestInitialize]
         public void Init()
         {
-            //TODO: Initial logic
+            Model = new Model();
+
+            if (!Model.GetConnectionStatus())
+                throw new Exception("Connect to Tekla model status is false!");
+
+            if(_insertedObjects == null)
+                _insertedObjects = new ConcurrentBag<ModelObject>();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            foreach(var mo in _insertedObjects)
+                mo?.Delete();
+
+            Model.CommitChanges();
+
+            _insertedObjects = null;
+        }
+
+        protected Beam GetBeam()
+        {
+            var beam = new Beam();
+            beam.Profile.ProfileString = "D100";
+            beam.StartPoint = new Point();
+            beam.EndPoint = new Point(1000, 0,0 );
+
+            if (beam.Insert())
+            {
+                _insertedObjects.Add(beam);
+                return beam;
+            }
+
+            return null;
+        }
+
+        protected Assembly CreateAssembly(Part mainPart, IEnumerable<Part> secondaries)
+        {
+            var assembly = mainPart.GetAssembly();
+
+            foreach(var part in secondaries)
+                assembly.Add(part);
+
+            return assembly;
         }
     }
 }
---
+++ src/Tests/RedButton.Tests.UnitTests/ExtensionsTests/AssemblyExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedButton.Common.TeklaStructures.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using Tekla.Structures.Model;
+
+namespace RedButton.Tests.UnitTests
+{
+    [TestClass]
+    public class GetPartsTests : BaseTest
+    {
+        [TestMethod("Get all part from assembly")]
+        public void GetAllPartFromAssemblyTest()
+        {
+            var beam1 = GetBeam();
+            var beam2 = GetBeam();
+            var beam3 = GetBeam();
+
+            var assembly = CreateAssembly(beam1, new List<Part> { beam2, beam3 });
+
+            var allParts = assembly.GetAllParts<Part>(true);
+
+            Assert.AreEqual(allParts.Count(), 3);
+        }
+
+        [TestMethod("Get all part from assembly without main part")]
+        public void GetAllPartFromAssemblyWithoutMainPartTest()
+        {
+            var beam1 = GetBeam();
+            var beam2 = GetBeam();
+            var beam3 = GetBeam();
+
+            var assembly = CreateAssembly(beam1, new List<Part> { beam2, beam3 });
+
+            var allParts = assembly.GetAllParts<Part>(false);
+
+            Assert.AreEqual(allParts.Count(), 2);
+        }
+    }
+}
