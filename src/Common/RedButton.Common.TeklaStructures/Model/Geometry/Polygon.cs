73ed306883a2755ed90a48e883629d585cdceee6
Andrei Stepanov <web.andrej@gmail.com>
2022-04-09 00:24:06 +0300

    Added IntersectionAllFacesSolid

---
+++ src/Common/RedButton.Common.TeklaStructures/Model/Geometry/Polygon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Tekla.Structures.Geometry3d;
+
+namespace RedButton.Common.TeklaStructures.Model.Geometry
+{
+    public struct Polygon
+    {
+        /// <summary>
+        /// Points
+        /// </summary>
+        public IEnumerable<Point> Points { get;}
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="points"></param>
+        public Polygon(IEnumerable<Point> points)
+        {
+            Points = points;
+        }
+    }
+}
