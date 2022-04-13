54b1374ec3cd50db8097905b4a96aa4145d1cd14
Nikita <razzak77@mail.ru>
2022-04-13 09:16:00 +0300

    Push

---
+++ src/Common/RedButton.Common.TeklaStructures/Model/MovingParts/PartMover.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Geometry3d;
+
+namespace RedButton.Common.TeklaStructures.Extensions
+{
+    /// <summary>
+    /// Class with methods to move parts.
+    /// </summary>
+    public class PartMover
+    {
+        /// <summary>
+        /// Move a beam normal to selected beam.
+        /// </summary>
+        /// <param name="baseObject"></param>
+        /// <param name="movingObject"></param>
+        public void MoveBeamNormalToBeam(Beam baseObject, Beam movingObject)
+        {
+            var model = new Model();
+
+            if (baseObject == null || movingObject == null || baseObject == movingObject)
+            {
+                Tekla.Structures.Model.Operations.Operation.DisplayPrompt("Выберите объекты.");
+                return;
+            }
+
+            var currentTP = model.GetWorkPlaneHandler().GetCurrentTransformationPlane();
+            var csBaseObject = baseObject.GetCoordinateSystem();
+            var tpBaseObject = new TransformationPlane(csBaseObject);
+            model.GetWorkPlaneHandler().SetCurrentTransformationPlane(tpBaseObject);
+            // Method .Select() refresh beam's coordinate sistem after a transfomation plane change.
+            baseObject.Select();
+            movingObject.Select();
+
+            if (movingObject.StartPoint.X > baseObject.GetSolid().MaximumPoint.X || movingObject.StartPoint.X < baseObject.GetSolid().MinimumPoint.X)
+            {
+                Tekla.Structures.Model.Operations.Operation.DisplayPrompt("Объект за пределами стационарного.");
+                return;
+            }
+            else
+            {
+                var MoveVector = new Vector(0, Math.Round(-movingObject.StartPoint.Y + baseObject.StartPoint.Y, 1), Math.Round(-movingObject.StartPoint.Z, 1));
+                Tekla.Structures.Model.Operations.Operation.MoveObject(movingObject, vec1);
+            }
+
+            model.GetWorkPlaneHandler().SetCurrentTransformationPlane(currentTP);
+            model.CommitChanges();
+        }
+    }
+}
