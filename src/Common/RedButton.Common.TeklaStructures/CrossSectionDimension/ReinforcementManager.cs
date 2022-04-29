a3271c888154e0f3c691f6e670f1e020411b02e9
Dimitrios <avitotempmail@mail.ru>
2022-04-30 00:46:03 +0300

    Создал новый класс для получения линий арматурных стержней. Работа не закончена.

---
+++ src/Common/RedButton.Common.TeklaStructures/CrossSectionDimension/ReinforcementManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using TSD = Tekla.Structures.Drawing;
+using TSM = Tekla.Structures.Model;
+using TSG = Tekla.Structures.Geometry3d;
+
+
+namespace RedButton.Common.TeklaStructures.CrossSectionDimension
+{
+    internal class ReinforcementManager
+    {
+        #region Properties
+        private TSM.Model _model;
+        private DrawingHandler _drawinghandler;
+        private ViewBase _pickedview;
+        private DrawingObject _drawingObject;
+        private TSM.Part _pickedpart;
+        private View _view;
+        public List<List<Point>> Points { get; private set; }
+        #endregion Properties
+
+        #region Constructors
+
+        public ReinforcementManager()
+        {
+            _model = new Model();
+            _drawinghandler = new DrawingHandler();
+        }
+
+        #endregion Constructors
+
+        #region Inteface
+
+        #endregion
+
+        #region Methods
+        public List<TSG.PolyLine> GetRebarsCurves(TSD.Part drawingPart)
+        {
+            List<TSG.PolyLine> polyLineList = new List<TSG.PolyLine>();
+            TSM.Part modelPart = _model.SelectModelObject(drawingPart.ModelIdentifier) as TSM.Part;
+            bool flag = (modelPart != null);
+            var reinforcementEnumerator = _view.GetAllObjects(typeof(ReinforcementBase));
+            if (flag)
+            {
+                while (reinforcementEnumerator.MoveNext())
+                {
+                    var temp = reinforcementEnumerator.Current as TSD.ReinforcementBase;
+                    if (temp != null)
+                    {
+                        var modelReinfocement = _model.SelectModelObject(temp.ModelIdentifier) as Reinforcement;
+                        if ((modelReinfocement != null) && (modelReinfocement.Father.Equals(modelPart)))
+                        {
+                            var rebarGeometry = modelReinfocement.GetRebarGeometries(Reinforcement.RebarGeometryOptionEnum.NONE);
+                            for (int i = 0; i < rebarGeometry.Count; i++)
+                            {
+                                var rebarShape = rebarGeometry[i] as RebarGeometry;
+                                if (rebarShape != null)
+                                {
+                                    polyLineList.Add(rebarShape.Shape);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return polyLineList;
+        }
+        public List<Point> GetIntersectedPoints(List<TSG.PolyLine> incomeList, double viewDepth)
+        {
+            List<Point> pointList = new List<Point>();
+            foreach (var item in incomeList)
+            {
+                if (item.Points.Count < 3)
+                {
+                    var startPoint = item.Points[0] as Point;
+                    var endPoint = item.Points[1] as Point;
+                    if (IsPlaneIntersectedByLine(startPoint, endPoint))
+                    {
+                        pointList.Add(new Point(startPoint.X, startPoint.Y, 0));
+                    }
+                }
+
+            }
+
+            return pointList;
+        }
+        public static bool IsPlaneIntersectedByLine(Point startPoint, Point endPoint)
+        {
+            bool flag1 = (endPoint.X - startPoint.X < 0.001) && (endPoint.Y - startPoint.Y < 0.001);
+            bool flag2 = (endPoint.Z * startPoint.Z < 1);
+            return (flag1 && flag2);
+        }
+        #endregion Methods
+    }
+}
+
